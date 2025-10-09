f1a2fbedf8f1a9eef0bfce0662a5312feee5b95c
zouhairmra <zouhairmrabet@gmail.com>
2025-10-09 10:23:19 +0300

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import statsmodels.api as sm
 
+
+
+
+def run_ols(df: pd.DataFrame, y: str, x: list):
+X = df[x]
+X = sm.add_constant(X)
+model = sm.OLS(df[y], X, missing='drop').fit()
+return model
