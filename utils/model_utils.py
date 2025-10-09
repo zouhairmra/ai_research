2b331883d67022f01580e61391d92a62611df2af
zouhairmra <zouhairmrabet@gmail.com>
2025-10-09 10:58:58 +0300

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import statsmodels.api as sm
 
+def run_ols(df: pd.DataFrame, y_col: str, x_cols: list):
+    """
+    Run OLS regression.
+    Returns a statsmodels regression results object.
+    """
+    X = sm.add_constant(df[x_cols])
+    y = df[y_col]
+    model = sm.OLS(y, X).fit()
+    return model
 
-
-
-def run_ols(df: pd.DataFrame, y: str, x: list):
-X = df[x]
-X = sm.add_constant(X)
-model = sm.OLS(df[y], X, missing='drop').fit()
-return model
+def run_logit(df: pd.DataFrame, y_col: str, x_cols: list):
+    """
+    Run logistic regression.
+    Returns a statsmodels Logit results object.
+    """
+    X = sm.add_constant(df[x_cols])
+    y = df[y_col]
+    model = sm.Logit(y, X).fit(disp=0)
+    return model
