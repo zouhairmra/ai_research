3dc4eb515307a887e6916f62482fc921b32d2cdc
zouhairmra <zouhairmrabet@gmail.com>
2025-10-09 10:23:42 +0300

    Update plot_utils.py

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,1 +1,9 @@
-
+import matplotlib.pyplot as plt
+import streamlit as st
+def reg_scatter_plot(df, y, x):
+fig, ax = plt.subplots()
+ax.scatter(df[x], df[y])
+ax.set_xlabel(x)
+ax.set_ylabel(y)
+ax.set_title(f"Scatter: {y} vs {x}")
+st.pyplot(fig)
